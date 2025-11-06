6b007f30eac009700106bf92e08eab3aba0309ef
gaurikathakur <gaurikathakur37@gmail.com>
2025-11-06 13:50:59 +0530

    Update backend with Gunicorn app instance

--- app.py
+++ app.py
@@ -57,9 +57,10 @@ def create_app():
 
     return app
 
-
-
 if __name__ == "__main__":
     app = create_app()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+else:
+    app = create_app()
+
